8af3c00deca444412c214fe7429e923e176e1596
Kopo Ntheledi <kopontheledi@gmail.com>
2023-06-06 16:11:59 +0200

    updates

---
+++ DWA5/scripts.js
@@ -0,0 +1,24 @@
+const form = document.querySelector("[data-form]");
+const result = document.querySelector("[data-result]");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const entries = new FormData(event.target);
+  const { dividend, divider } = Object.fromEntries(entries);
+    //When empty strings are submitted
+  if (dividend === "" || divider === "" ) {
+ result.innerText = "Division is not performed. Both values are required in the inputs. Try again"
+  }
+  //when a number that is less than 0 is submitted
+  else if (dividend < 0  || divider < 0  ) {
+    result.innerText = "Division not performed. Invalid number provided. Try again";
+    console.error()
+    //when an input is not a number
+  } else if(isNaN(dividend) || (isNaN(divider))){
+    console.error (new error(`Something critical went wrong. Please reload the page!`));
+    document.body.innerHTML = '<h1>Something critical went wrong. Please reload the page!</h1>'
+  }
+  // to zero decimal
+  else{
+  result.innerText = Math.floor(dividend / divider);
+  }
+});
