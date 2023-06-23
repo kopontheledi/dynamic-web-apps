9827c9a4459fb14b6b0aadc1681126b24ce3dc33
Kopo Ntheledi <kopontheledi@gmail.com>
2023-06-23 15:35:52 +0200

    updates

--- DWA10/Tally count/script.js
+++ DWA10/Tally count/script.js
@@ -1,73 +1,67 @@
-// const MAX_NUMBER =15
-// const MIN_NUMBER =-5
-
-// const number= document.querySelector ('[data-key="number"]')
-// const subtract=  document.querySelector ('[data-key="subtract"]')
-// const add=  document.querySelector ('[data-key="add"]')
-
-// const subtractHandler = ()  =>{
-// const newValue= parseInt(number.value) - 1
-// number.value = newValue
-
-// if ( add.disabled === true) {
-//     add.disabled = false
-// }
-
-// if (newValue <= MIN_NUMBER) {
-//     subtract.disabled = true
-// }
-// }
-
-// const addHandler = ()  =>{
-//     const newValue= parseInt(number.value) + 1
-// number.value = newValue
-
-
-// if ( subtract.disabled === true) {
-//     subtract.disabled = false
-// }
-
-// if (newValue >= MAX_NUMBER) {
-//     add.disabled = true
-// }
-// }
-
-
-// subtract.addEventListener ('click', subtractHandler )
-
-
-// add.addEventListener  ('click', addHandler )
-
-const MAX_NUMBER = 15;
-const MIN_NUMBER = -5;
-
-const number = document.querySelector('[data-key="number"]');
-const subtract = document.querySelector('[data-key="subtract"]');
-const add = document.querySelector('[data-key="add"]');
-const reset = document.querySelector('[data-key="reset"]');
-const message = document.querySelector('[data-key="message"]');
-
-const updateCounter = (newValue) => {
-  number.value = newValue;
-  subtract.disabled = newValue <= MIN_NUMBER;
-  add.disabled = newValue >= MAX_NUMBER;
+/**
+  * Maximum allowed number.
+  * @constant {number}
+  */
+const MAX_NUMBER = 15
+/**
+ * Minimum allowed number.
+ * @constant {number}
+ */
+const MIN_NUMBER = -5
+/**
+ * The number input element.
+ * @type {HTMLInputElement}
+ */
+const number = document.querySelector('[data-key="number"]')
+/**
+ * The subtract button element.
+ * @type {HTMLButtonElement}
+ */
+const subtract = document.querySelector('[variant="danger"]')
+/**
+ * The add button element.
+ * @type {HTMLButtonElement}
+ */
+const add = document.querySelector('[variant="success"]')
+const reset = document.querySelector('[variant="default"]')
+/**
+ * Event handler for the subtract button click event.
+ * @prop {Function} subtractHandler - Event handler for the subtract button click event.
+ * @param {Event} event - The click event object.
+ */
+const subtractHandler = (event) => {
+    /**
+     * The new value after subtracting one from the current number value.
+     * @type {number}
+     * @prop {number} newValue - The new value after subtracting one.
+     */
+    const newValue = parseInt(number.value) - 1;
+    number.value = newValue;
+    if (newValue <= MIN_NUMBER) {
+        subtract.disabled = true;
+    } else {
+        subtract.disabled = false;
+    }
 };
-
-const subtractHandler = () => {
-  const newValue = parseInt(number.value) - 1;
-  updateCounter(newValue);
-};
-
 const addHandler = () => {
-  const newValue = parseInt(number.value) + 1;
-  updateCounter(newValue);
-};
-
+    const newValue = parseInt(number.value) + 1
+    number.value = newValue
+    if (newValue >= MAX_NUMBER) {
+        add.disabled = true
+    } else {
+        add.disabled = false
+    }
+}
 const resetHandler = () => {
-  updateCounter(0);
-  message.textContent = "The counter has been reset.";
+    number.value = 0;
+    add.disabled = false;
+    subtract.disabled = false;
+    showMessage("Reset has been pressed")
+}
+const showMessage = (message) => {
+    alert(message);
+    message.disabled = false
 };
-
-subtract.addEventListener('click', subtractHandler);
-add.addEventListener('click', addHandler);
-reset.addEventListener('click', resetHandler);
+subtract.addEventListener('click', subtractHandler)
+add.addEventListener('click', addHandler)
+reset.addEventListener('click', resetHandler )
