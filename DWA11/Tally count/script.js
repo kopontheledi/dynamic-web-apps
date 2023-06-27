31b0d0c8a6cb86fbb25eeecdcf21baa7fc5f692b
Kopo Ntheledi <kopontheledi@gmail.com>
2023-06-27 11:46:42 +0200

    updates

--- DWA11/Tally count/script.js
+++ DWA11/Tally count/script.js
@@ -38,36 +38,97 @@
 
 // add.addEventListener  ('click', addHandler )
 
-const MAX_NUMBER = 15;
-const MIN_NUMBER = -5;
+// const MAX_NUMBER = 15;
+// const MIN_NUMBER = -5;
 
-const number = document.querySelector('[data-key="number"]');
-const subtract = document.querySelector('[data-key="subtract"]');
-const add = document.querySelector('[data-key="add"]');
-const reset = document.querySelector('[data-key="reset"]');
-const message = document.querySelector('[data-key="message"]');
+// const number = document.querySelector('[data-key="number"]');
+// const subtract = document.querySelector('[data-key="subtract"]');
+// const add = document.querySelector('[data-key="add"]');
+// const reset = document.querySelector('[data-key="reset"]');
+// const message = document.querySelector('[data-key="message"]');
 
-const updateCounter = (newValue) => {
-  number.value = newValue;
-  subtract.disabled = newValue <= MIN_NUMBER;
-  add.disabled = newValue >= MAX_NUMBER;
+// const updateCounter = (newValue) => {
+//   number.value = newValue;
+//   subtract.disabled = newValue <= MIN_NUMBER;
+//   add.disabled = newValue >= MAX_NUMBER;
+// };
+
+// const subtractHandler = () => {
+//   const newValue = parseInt(number.value) - 1;
+//   updateCounter(newValue);
+// };
+
+// const addHandler = () => {
+//   const newValue = parseInt(number.value) + 1;
+//   updateCounter(newValue);
+// };
+
+// const resetHandler = () => {
+//   updateCounter(0);
+//   message.textContent = "The counter has been reset.";
+// };
+
+// subtract.addEventListener('click', subtractHandler);
+// add.addEventListener('click', addHandler);
+// reset.addEventListener('click', resetHandler);
+
+// Define the reducer function
+const reducer = (state = { count: 0 }, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { count: state.count + 1 };
+    case 'DECREMENT':
+      return { count: state.count - 1 };
+    case 'RESET':
+      return { count: 0 };
+    default:
+      return state;
+  }
 };
 
-const subtractHandler = () => {
-  const newValue = parseInt(number.value) - 1;
-  updateCounter(newValue);
+// Create the store
+const createStore = (reducer) => {
+  let state = reducer(undefined, {});
+  const subscribers = [];
+
+  const getState = () => state;
+
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    subscribers.forEach((subscriber) => subscriber());
+  };
+
+  const subscribe = (subscriber) => {
+    subscribers.push(subscriber);
+    return () => {
+      const index = subscribers.indexOf(subscriber);
+      if (index !== -1) {
+        subscribers.splice(index, 1);
+      }
+    };
+  };
+
+  return {
+    getState,
+    dispatch,
+    subscribe,
+  };
 };
 
-const addHandler = () => {
-  const newValue = parseInt(number.value) + 1;
-  updateCounter(newValue);
-};
+// Create the store instance
+const store = createStore(reducer);
 
-const resetHandler = () => {
-  updateCounter(0);
-  message.textContent = "The counter has been reset.";
-};
+// Subscribe to state changes
+const unsubscribe = store.subscribe(() => {
+  const state = store.getState();
+  console.log('New state:', state);
+});
 
-subtract.addEventListener('click', subtractHandler);
-add.addEventListener('click', addHandler);
-reset.addEventListener('click', resetHandler);
+// Dispatch actions
+store.dispatch({ type: 'INCREMENT' }); // Increment the counter
+store.dispatch({ type: 'INCREMENT' }); // Increment the counter
+store.dispatch({ type: 'DECREMENT' }); // Decrement the counter
+store.dispatch({ type: 'RESET' }); // Reset the counter
+
+// Unsubscribe from state changes
+unsubscribe();
