3e75f82c8e5fa0bdd98c87b9bca04d1d8ba8f80f
Kopo Ntheledi <kopontheledi@gmail.com>
2023-06-29 10:31:33 +0200

    updates

--- DWA13/script.js
+++ DWA13/script.js
@@ -28,3 +28,78 @@ names.forEach((name) => {
   // Exercise 5: Sorting provinces alphabetically
   const sortedProvinces = provinces.sort();
   console.log(sortedProvinces);
+
+  //NUMBER 2
+
+  const products = [
+    { product: 'banana', price: "2" },
+    { product: 'mango', price: 6 },
+    { product: 'potato', price: ' ' },
+    { product: 'avocado', price: "8" },
+    { product: 'coffee', price: 10 },
+    { product: 'tea', price: '' },
+  ];
+  
+  // Exercise 1: Logging each product name
+  products.forEach((product) => {
+    console.log(product.product);
+  });
+  
+  // Exercise 2: Filtering out products with names longer than 5 characters
+  const filteredProducts = products.filter((product) => {
+    return product.product.length <= 5;
+  });
+  console.log(filteredProducts);
+  
+  // Exercise 3: Converting prices to numbers and removing products without prices,
+  // then calculating the combined price using reduce
+  const combinedPrice = products
+    .filter((product) => product.price !== '' && !isNaN(product.price))
+    .map((product) => {
+      product.price = Number(product.price);
+      return product;
+    })
+    .reduce((acc, product) => {
+      return acc + product.price;
+    }, 0);
+  console.log(combinedPrice);
+  
+  // Exercise 4: Concatenating all product names using reduce
+  const concatenatedNames = products.reduce((acc, product, index) => {
+    acc += product.product;
+    if (index < products.length - 1) {
+      acc += ', ';
+    }
+    return acc;
+  }, '');
+  console.log(concatenatedNames);
+  
+  // Exercise 5: Calculating the highest and lowest-priced items using reduce
+  const priceStats = products.reduce(
+    (acc, product) => {
+      if (product.price !== '' && !isNaN(product.price)) {
+        const price = Number(product.price);
+        if (price > acc.highestPrice) {
+          acc.highestPrice = price;
+          acc.highestProduct = product.product;
+        }
+        if (price < acc.lowestPrice) {
+          acc.lowestPrice = price;
+          acc.lowestProduct = product.product;
+        }
+      }
+      return acc;
+    },
+    { highestPrice: -Infinity, lowestPrice: Infinity, highestProduct: '', lowestProduct: '' }
+  );
+  const priceStatsString = `Highest: ${priceStats.highestProduct}. Lowest: ${priceStats.lowestProduct}`;
+  console.log(priceStatsString);
+  
+  // Exercise 6: Recreating the object with modified keys using Object.entries and reduce
+  const recreatedProducts = Object.entries(products).reduce((acc, [key, value]) => {
+    const modifiedKey = key === 'product' ? 'name' : key === 'price' ? 'cost' : key;
+    acc[modifiedKey] = value;
+    return acc;
+  }, {});
+  console.log(recreatedProducts);
+  
